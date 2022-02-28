91bb3b3c46b724faacd41ef588daa36cbf496339
Imanprokeren <98250257+Imanprokeren@users.noreply.github.com>
2022-02-28 14:50:33 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,6 +1,6 @@
 const PhoneNumber = require('awesome-phonenumber')
 async function handler(m) {
-                let vcard = "BEGIN:VCARD\nVERSION:3.0\nN:1;ZaBot-MD;;;\nFN:ZaBot-MD\nitem1.TEL;waid=6289522724388:+62 895-2272-4388\nitem1.X-ABLabel:Ponsel\nX-WA-BIZ-NAME:ZaBot-MD\nEND:VCARD"
+                let vcard = "BEGIN:VCARD\nVERSION:3.0\nN:1;ImanOffc;;;\nFN:ImanOffc\nitem1.TEL;waid=6289522724388:+62 895-2272-4388\nitem1.X-ABLabel:Ponsel\nX-WA-BIZ-NAME:ImanOffc\nEND:VCARD"
                 conn.sendMessage(m.chat, { contacts: { contacts: [{ vcard }] } }, { quoted: m })
 }
 handler.help = ['owner', 'creator']
